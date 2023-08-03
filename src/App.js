b1aed70a07cd0c95f6ae3ea4dc51608aafd2e71f
Hemant Bansal <bhemant400@gmail.com>
2023-08-03 16:51:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import {MainLayout} from './styles/layout'
 import Orb from './components/Orb/orb'
 import Dashboard from './components/Dashboard/Dashboard';
 import Income from './components/Income/Income'
+
 import Expenses from './components/Expenses/Expenses';
 import Transactions from './components/Transactions/Transactions'
 
 import Navigation from "./components/Navigations/Navigation";
 function App() {
   const [active, setActive] = useState(1)
-
+  
   
   
   const displayData = () => {
